837ee9aae7ed2cf4c672d153eaddd110254cd0f6
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-12-11 15:59:56 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+function App() {
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    "Acredite em si mesmo, pois a confiança é o primeiro passo para o sucesso.",
+    "Cada novo dia é uma nova oportunidade para alcançar seus objetivos.",
+    "O sucesso nasce do esforço contínuo e da determinação inabalável.",
+    "Não tema os desafios, veja-os como oportunidades de crescimento.",
+    "Seja a mudança que você deseja ver no mundo e na sua vida.",
+    "O caminho para o sucesso é pavimentado com trabalho árduo e perseverança.",
+    "A motivação é a faísca que acende a chama do progresso.",
+    "Transforme seus sonhos em metas e suas metas em realidade.",
+    "A jornada pode ser difícil, mas a vitória é sempre gratificante.",
+    "A única maneira de alcançar o impossível é acreditar que é possível.",
+    "Não espere por oportunidades, crie-as com seu esforço e dedicação.",
+    "Cada fracasso é uma lição valiosa no caminho para o sucesso.",
+    "Mantenha o foco no seu objetivo e não se distraia com as distrações.",
+    "O otimismo é a chave que abre as portas para um futuro promissor.",
+    "A força não vem do corpo, mas da vontade de nunca desistir.",
+    "A vida é 10% do que acontece conosco e 90% de como reagimos a isso.",
+    "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+    "Não deixe que o medo do fracasso impeça você de tentar algo novo.",
+    "A persistência supera o talento quando o talento não persiste.",
+    "Celebre as pequenas vitórias, pois elas são os degraus para conquistas maiores."
+  ];
+
+
+  function quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+
+    setTextoFrase(' " ' + frases[numeroAleatorio] + ' " ');
+    setImg(require('./src/biscoitoAberto.png'));
+  }
+
+
+  function reiniciar() {
+
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image
+        source={img}
+        style={styles.img}
+      />
+
+      <Text style={styles.textofrase}> {textoFrase} </Text>
+
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto} >Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={reiniciar}>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, { color: '#121212' }]} >Reiniciar biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +74,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
+})
+
+export default App;
---
+++ README.md
@@ -0,0 +1,111 @@
+<p align="center">
+   <img src="https://media.giphy.com/media/R1uM7DRJM1G6MDxt3E/giphy.gif" alt="motivacao" width="280"/>
+</p>
+
+<p align="right">
+   <img src="https://media.giphy.com/media/YWwFqnCFY8IFfDJxey/giphy.gif" alt="motivacao" width="130"/>
+</p>
+
+<h1 align="center"></h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/agostinhomarcia/diety?color=4266f5">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/agostinhomarcia/diety?color=4266f5">
+
+  <img alt="License" src="https://img.shields.io/github/license/agostinhomarcia/diety?color=4266f5">
+
+   <img alt="Github issues" src="https://img.shields.io/github/issues/agostinhomarcia/diety?color=4266f5" />
+
+   <img alt="Github forks" src="https://img.shields.io/github/forks/agostinhomarcia/diety?color=4266f5" />
+
+   <img alt="Github stars" src="https://img.shields.io/github/stars/agostinhomarcia/diety?color=4266f5" /> 
+</p>
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/agostinhomarcia" target="_blank">Author</a>&#xa0; | &#xa0
+  <a href="#" target="_blank" rel="noopener noreferrer">Projeto</a>
+</p>
+
+<br>
+
+## :dart: About
+
+<h4 align="center"> Biscoito da Sorte </h4>
+
+<p align="center">
+  <img src="./src/sorte.gif" alt="Money" />
+  
+</p>
+
+<p align="center">
+   Desenvolver uma aplicação de cadastro de refeições, que contém as seguintes funcionalidades:
+
+- Adicionar uma nova refeição
+- Editar uma refeição
+- Remover uma refeição da listagem
+- Mostrar as estatísticas do progresso da dieta
+- Navegação entre telas em pilha
+- Armazenamento local das refeições
+
+Fixando conhecimentos nos conceitos:
+
+- Estados
+- Imutabilidade do estado
+- Listas e chaves no React Native
+- Propriedades
+- Componentização
+- Parâmetros de navegação</p>
+</p>
+
+## :sparkles: Features
+
+:heavy_check_mark: Feature 1;\
+:heavy_check_mark: Feature 2;\
+:heavy_check_mark: Feature 3;
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [React-Native](https://reactnative.dev/docs/getting-started)
+- [React.js](https://legacy.reactjs.org/)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting
+
+```bash
+# Clone this project
+$ git clone https://github.com/agostinhomarcia/diety.git
+# Access
+$ cd diety
+# Install dependencies
+$ yarn or npm install
+# Run the project
+$ yarn start or npm start
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License
+
+This project is under the [MIT license](./License).
+
+Made with love by [Márcia Agostinho](https://github.com/agostinhomarcia) 🚀.
+
+<p align="center">
+   <img src="https://media.giphy.com/media/v3ADik6tAm07Laxgdd/giphy.gif" alt="motivacao" width="450"/>
+</p>
+
+&#xa0;
+
+<a href="#top">Back to top </a>
